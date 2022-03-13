22217707b3b2e2fa2a09adbe812353d41bf3b0c1
杨光辉 <gh.yang@outlook.com>
2022-03-14 00:27:04 +0800

    docs

---
+++ docs/conf/config.map.js
@@ -0,0 +1,55 @@
+/*
+ * @Author: 杨光辉(GerhardYang)
+ * @Date: 2022-03-13 22:25:35
+ * @LastEditors: 杨光辉(GerhardYang)
+ * @LastEditTime: 2022-03-13 23:14:30
+ * @Description: file contentconst
+ * @Copyright: 超图软件华中平台客户中心 (SuperMap Software Co., Ltd. -Central China Platform)
+ */
+window.supermap_config.map = {
+  name: "基本使用",
+  demos: [
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+    {
+      name: "初始化场景",
+      url: "/demos/InitViewer.html",
+      imgSrc: "/imgs/InitViewer.png",
+      detail: "使用new Cesium.Viewer()初始化场景",
+    },
+  ],
+};
