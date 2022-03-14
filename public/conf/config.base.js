d59624c24f55cb6e97ecf8fa3eaa55d75246e49f
GerhardYang <gh.yang@outlook.com>
2022-03-14 13:42:19 +0800

    修改资源引入规则

--- public/conf/config.base.js
+++ public/conf/config.base.js
@@ -2,7 +2,7 @@
  * @Author: 杨光辉(GerhardYang)
  * @Date: 2022-03-13 22:25:35
  * @LastEditors: 杨光辉(GerhardYang)
- * @LastEditTime: 2022-03-13 23:14:21
+ * @LastEditTime: 2022-03-14 13:36:27
  * @Description: file contentconst
  * @Copyright: 超图软件华中平台客户中心 (SuperMap Software Co., Ltd. -Central China Platform)
  */
@@ -12,44 +12,44 @@ window.supermap_config.base = {
   demos: [
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
     {
       name: "初始化场景",
-      url: "/demos/InitViewer.html",
-      imgSrc: "/imgs/InitViewer.png",
+      url: "./demos/InitViewer.html",
+      imgSrc: "./imgs/InitViewer.png",
       detail: "使用new Cesium.Viewer()初始化场景",
     },
   ],
