0c614cb21fd1a7a8a677c1e8f47765e8f354f217
杨光辉 <gh.yang@outlook.com>
2022-03-20 22:11:21 +0800

    百度地图加载

--- public/conf/config.map.js
+++ public/conf/config.map.js
@@ -2,7 +2,7 @@
  * @Author: 杨光辉(GerhardYang)
  * @Date: 2022-03-13 22:25:35
  * @LastEditors: 杨光辉(GerhardYang)
- * @LastEditTime: 2022-03-20 00:30:45
+ * @LastEditTime: 2022-03-20 21:45:11
  * @Description: file contentconst
  * @Copyright: 超图软件华中平台客户中心 (SuperMap Software Co., Ltd. -Central China Platform)
  */
@@ -26,6 +26,12 @@ window.supermap_config.map = {
       url: "./demos/map/MVTmap.html",
       imgSrc: "./imgs/map/MVTmap.png",
       detail: "加载iServer发布的MVT矢量瓦片",
+    },
+    {
+      name: "加载百度地图",
+      url: "./demos/map/TDTmap3.html",
+      imgSrc: "./imgs/map/TDTmap3.png",
+      detail: "加载在线百度地图瓦片并纠偏到wgs84坐标系",
     },
   ],
 };
